18970794d8753d0f734f76c4f4406f1873981931
marco <marco@joinder.me>
2022-02-08 13:39:33 -0300

    Upload Avatar

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '83fbb03647fb15a05edc3ba7b496784d',
+    expiresIn: '1d',
+  },
+};
--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,6 +1,7 @@
 import AppError from '@shared/errors/AppError';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '@config/auth';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -29,9 +30,9 @@ class CreateSessionsService {
       throw new AppError('Incorrect email/password combination', 401);
     }
 
-    const token = sign({}, '83fbb03647fb15a05edc3ba7b496784d', {
+    const token = sign({}, authConfig.jwt.secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn: authConfig.jwt.expiresIn,
     });
 
     return {
