74ff54b9c2f259ea0571d962497cf44d7c0fded5
marco <marco@joinder.me>
2022-02-10 20:51:06 -0300

    Implementação das regras para atualização dos dados do usuário

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -39,7 +39,7 @@ class SendForgotPasswordEmailService {
         file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          link: `http://localhost:3000/reset_password?token=${token}`,
+          link: `http://localhost:3000/reset_password?token=${token.token}`,
         },
       },
     });
